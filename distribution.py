b45a3563cc372b01590ea2dfee8cc4bc48634c97
ellaedmonds <35499735+ellaedmonds@users.noreply.github.com>
2018-10-02 11:27:31 -0400

    Updated from Brython Server: 10/2/2018 11:27:30 AM

--- distribution.py
+++ distribution.py
@@ -36,10 +36,24 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+a = input ('Please enter a string of text (the bigger the better):')
+print('The distribution of characters in "'+a+'" is:')
 
-
+letters = []
+freq = []
+letter = ""
 for c in "abcdefghijklmnopqrstuvwxyz":
-    
+    if c in a:
+        letters.append(c)
+        freq.append(a.count(c))
+
+print(letters)
+print(freq)
+letters2 = str(letters)
+freq2 = int(freq)
+
+for letter in letters:
+    print('{letter}'*freq2)
 
 
 
